89b9e4124b6aedf88fdf6386509ac95f5bf39a72
dlevine3 <danlevine13@gmail.com>
2013-10-01 01:08:26 -0700

    Fixed the password checking

--- client/managers/signup/signup.js
+++ client/managers/signup/signup.js
@@ -6,8 +6,42 @@ Template.signup.helpers({
 		return Session.get('isEmployer');
 	}
 });
+function checkPassword(str)
+{
+   	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+   	return re.test(str);
+};
+
+function checkForm(form)
+{
+	if(form.username.value == "") {
+    	alert("Error: Username cannot be blank!");
+    	form.username.focus();
+    	return false;
+    }
+    re = /^\w+$/;
+    if(!re.test(form.username.value)) {
+    	alert("Error: Username must contain only letters, numbers and underscores!");
+    	form.username.focus();
+    	return false;
+    }
+    if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+    	if(!checkPassword(form.pwd1.value)) {
+       		alert("The password you have entered is not valid!");
+       		form.pwd1.focus();
+       		return false;
+    	}
+    } 
+    else {
+    	alert("Error: Please check that you've entered and confirmed your password!");
+    	form.pwd1.focus();
+      	return false;
+   	}
+   	return true;
+};
 
 Template.signup.events({
+
 	'click .student-button': function() {
 		Session.set('isStudent', true);
 		Session.set('isEmployer', false);
@@ -16,6 +50,8 @@ Template.signup.events({
 		Session.set('isEmployer', true);
 		Session.set('isStudent', false);
 	},
+
+	
 	'click #register': function(e) {
 		e.preventDefault();
 
@@ -30,15 +66,17 @@ Template.signup.events({
 		{
 			console.log('they dont match');
 		}
-	},
+
+
+	},/*
 	'keypress #confirm': function(e) {
 		// FIX THIS
 		// 
 		// ========
-		// var password = $('#password').val();
-		// if (e.target.value != password) {
-		// 	console.log('no match');
-		// }
-		// console.log(password, e.target.value);
-	}
+		var password = $('#password').val();
+		if (e.target.value != password) {
+			console.log('no match');
+		}
+		console.log(password, e.target.value);
+	}*/
 });
