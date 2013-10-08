1b987e03a4ca8979f94f4e1091a62fe702f28984
Tim Whitaker <tmwhtkr@gmail.com>
2013-10-08 12:18:28 -0700

    lots of updates to the homepage. basic layout is there. can now move to other functionality

--- client/router.js
+++ client/router.js
@@ -1,7 +1,7 @@
 Router.configure({layout: 'layout'});
 
 Router.map(function() {
-	this.route('main', {path: '/'});
+	this.route('home', {path: '/'});
 	this.route('about');
 	this.route('signup');
 	this.route('contact');
