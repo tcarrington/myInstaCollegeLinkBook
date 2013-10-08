411a948754532a91e22343bc31cdf1f8bfc26571
Tim Whitaker <tmwhtkr@gmail.com>
2013-10-08 13:51:52 -0700

    tried some router trickery but no dice.

--- client/main.js
+++ client/main.js
@@ -2,7 +2,6 @@ Meteor.startup(function() {
 	Session.setDefault('currentPage', 'Home');
 	var currentPage = Session.get('currentPage');
 	$('.nav li a:contains("' + currentPage + '")').addClass('active');
-
 });
 
 Deps.autorun(function() {
