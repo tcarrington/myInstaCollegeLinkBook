a5dd5e098718323ec8a3c6975230ff4fb18d3e06
Tim Whitaker <tmwhtkr@gmail.com>
2013-10-08 13:13:03 -0700

    session variable for current page is now set whenever the router handles a new page

--- client/router.js
+++ client/router.js
@@ -1,8 +1,25 @@
 Router.configure({layout: 'layout'});
 
 Router.map(function() {
-	this.route('home', {path: '/'});
-	this.route('about');
-	this.route('signup');
-	this.route('contact');
+	this.route('home', {
+				path: '/',
+			    onBeforeRun: function() {
+			    	Session.set('currentPage', 'Home');	
+			    }
+	});
+	this.route('about', {
+				onBeforeRun: function() {
+					Session.set('currentPage', 'About'); 
+				}
+	});
+	this.route('signup', {
+				onBeforeRun: function() {
+					Session.set('currentPage', 'Signup');
+				}
+	});
+	this.route('contact', {
+				onBeforeRun: function() {
+					Session.set('currentPage', 'Contact');
+				}
+	});
 });
